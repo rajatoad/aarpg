159ccc2e4ee4af223563d0e01410ed5f20388cac
BrianA <bravoromeoalpharomeo@gmail.com>
2025-11-08 16:46:43 -0500

    inventory

--- GUI/pause_menu/pause_menu.gd
+++ GUI/pause_menu/pause_menu.gd
@@ -1,7 +1,11 @@
 extends CanvasLayer
 
-@onready var button_save: Button = $VBoxContainer/Button_Save
-@onready var button_load: Button = $VBoxContainer/Button_Load
+signal shown
+signal hidden
+
+@onready var button_save: Button = $Control/HBoxContainer/Button_Save
+@onready var button_load: Button = $Control/HBoxContainer/Button_Load
+@onready var item_description: Label = $Control/ItemDescription
 
 var is_paused : bool = false
 
@@ -25,12 +29,13 @@ func show_pause_menu() -> void:
 	get_tree().paused = true
 	visible = true
 	is_paused = true
-	button_save.grab_focus()
+	shown.emit()
 
 func hide_pause_menu() -> void:
 	get_tree().paused = false
 	visible = false
 	is_paused = false
+	hidden.emit()
 
 
 func _on_save_pressed() -> void:
@@ -46,4 +51,6 @@ func _on_load_pressed() -> void:
 	SaveManager.load_game()
 	await LevelManager.level_load_started
 	hide_pause_menu()
-	pass
+
+func update_item_description( new_description : String) -> void:
+	item_description.text = new_description
