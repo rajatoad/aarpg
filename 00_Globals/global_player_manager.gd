b801e70bd1f7c6f42bab65f332de6229d572b268
Brian <b.arayathel@gmail.com>
2025-11-09 11:40:23 -0500

    item pickup process

--- 00_Globals/global_player_manager.gd
+++ 00_Globals/global_player_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const PLAYER = preload("uid://bf4esf57fcnh8")
+const INVENTORY_DATA : InventoryData = preload("res://GUI/pause_menu/Inventory/player_inventory.tres")
 
 var player : Player
 var player_spawned : bool = false
--- GUI/pause_menu/Inventory/Scripts/inventory_data.gd
+++ GUI/pause_menu/Inventory/Scripts/inventory_data.gd
@@ -2,5 +2,20 @@ class_name InventoryData extends Resource
 
 @export var slots : Array[SlotData]
 
-func add_item() -> void:
-	pass
+func add_item(item: ItemData, count : int = 1) -> bool:
+	for s in slots:
+		if s:
+			if s.item_data == item:
+				s.quantity += count
+				return true
+	
+	for i in slots.size():
+		if slots[i] == null:
+			var new_slot = SlotData.new()
+			new_slot.item_data = item
+			new_slot.quantity = count
+			slots[i] = new_slot
+			return true
+	
+	print("inventory was full")
+	return false
